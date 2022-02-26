2a7ec1e0affe684eaffb9ecc4322d8012a26d74b
ramanverma2k <ramanverma4183@gmail.com>
2022-02-26 12:41:27 +0530

    add flutter_riverpod package for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mastermind/homepage.dart';
+import 'package:mastermind/utils/setup_providers.dart';
 
 void main() {
-  runApp(const MyApp());
+  setUpProviders();
+
+  runApp(
+    // For widgets to be able to read providers, we need to wrap the entire
+    // application in a "ProviderScope" widget.
+    // This is where the state of our providers will be stored.
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
