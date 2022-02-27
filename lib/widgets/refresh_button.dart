888d1ff0f051d5e38db1996ebeb22d1f4d9c7a36
ramanverma2k <ramanverma4183@gmail.com>
2022-02-27 10:40:15 +0530

    add refresh lottie animation

--- lib/widgets/refresh_button.dart
+++ lib/widgets/refresh_button.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 import 'package:mastermind/utils/board_state.dart';
 
-class RefreshButton extends ConsumerWidget {
+class RefreshButton extends ConsumerStatefulWidget {
   const RefreshButton({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RefreshButton> createState() => _RefreshButtonState();
+}
+
+class _RefreshButtonState extends ConsumerState<RefreshButton>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final bool popUpState = ref.watch(isPopupOpen.state).state;
 
     return !popUpState
         ? GestureDetector(
             onTap: () {
-              // Reset the board state
-              ref.refresh(boardState);
+              // When the animation is complete reset the state of the board along with animation state.
+              _controller.forward().whenComplete(
+                () {
+                  // Reset the animation state
+                  _controller.reset();
+
+                  // Reset the board state
+                  ref.refresh(boardState);
+                },
+              );
             },
-            child: const Icon(
-              Icons.refresh_rounded,
-              color: Colors.black,
+            child: Lottie.asset(
+              'assets/lottie/refresh.json',
+              controller: _controller,
+              onLoaded: (composition) {
+                _controller.duration = composition.duration;
+              },
             ),
           )
         : const SizedBox.shrink();
