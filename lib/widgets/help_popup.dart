88e2eb275cb134f9d9ef03dcdcd1a4fa3992c8bc
ramanverma2k <ramanverma4183@gmail.com>
2022-03-12 13:41:26 +0530

    help_popup.dart: finish styling

--- lib/widgets/help_popup.dart
+++ lib/widgets/help_popup.dart
@@ -14,6 +14,8 @@ class HelpPopup extends ConsumerWidget {
 
     final darkTheme = ref.watch(isDarkTheme);
 
+    final width = MediaQuery.of(context).size.width;
+
     return help
         ? Scaffold(
             backgroundColor: Colors.transparent,
@@ -27,117 +29,134 @@ class HelpPopup extends ConsumerWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     DecoratedBox(
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
+                      decoration: BoxDecoration(
+                        color: darkTheme ? Colors.transparent : Colors.white,
+                        border: Border.all(
+                          style: BorderStyle.solid,
+                          color: darkTheme ? Colors.white : Colors.black,
+                          width: 2,
+                        ),
                       ),
-                      child: SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.5,
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text(
-                              'Game Rules',
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.62,
+                          width: width < 390 ? width * 0.8 : width * 0.6,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Game Rules',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      darkTheme ? Colors.white : Colors.black,
+                                ),
                               ),
-                            ),
-                            const Text(
-                              'Objective',
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
+                              const SizedBox(height: 10),
+                              Text(
+                                'Objective',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      darkTheme ? Colors.white : Colors.black,
+                                ),
                               ),
-                            ),
-                            Wrap(
-                              children: [
-                                const Text(
-                                    'A secret combination of 4 colors is selected and you have to guess that combination in 8 or fewer tries to win.'),
-                                const Text(
-                                  'How to play',
-                                  style: TextStyle(
-                                    fontSize: 25,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
+                              const SizedBox(height: 10),
+                              const Text(
+                                  'A secret combination of 4 colors is selected and you have to guess that combination in 8 or fewer tries to win.'),
+                              const SizedBox(height: 10),
+                              Text(
+                                'How to play',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      darkTheme ? Colors.white : Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              const Text(
+                                  'From top to bottom, at each row, click on a circle and pick a color. After filling all circles in a row, you can check your guess.'),
+                              const SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: darkTheme
+                                          ? Colors.white
+                                          : Colors.black,
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                    child: const SizedBox(
+                                      height: 10,
+                                      width: 10,
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  width: 20,
-                                ),
-                                const Text(
-                                    'From top to bottom, at each row, click on a circle and pick a color. After filling all circles in a row, you can check your guess.'),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    DecoratedBox(
-                                      decoration: BoxDecoration(
-                                        color: darkTheme
-                                            ? Colors.white
-                                            : Colors.black,
-                                        borderRadius: BorderRadius.circular(50),
-                                      ),
-                                      child: const SizedBox(
-                                        height: 10,
-                                        width: 10,
-                                      ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  const Flexible(
+                                    child: Text(
+                                        'A grey circle means that the color exists in the combination but the position is not correct.'),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: darkTheme
+                                          ? Colors.white
+                                          : Colors.black,
+                                      borderRadius: BorderRadius.circular(50),
                                     ),
-                                    const SizedBox(
-                                      width: 20,
+                                    child: const SizedBox(
+                                      height: 10,
+                                      width: 10,
                                     ),
-                                    const Text(
-                                        'An grey circle means that the color exists in the combination but the position is not correct.')
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    DecoratedBox(
-                                      decoration: BoxDecoration(
-                                        color: darkTheme
-                                            ? Colors.white
-                                            : Colors.black,
-                                        borderRadius: BorderRadius.circular(50),
-                                      ),
-                                      child: const SizedBox(
-                                        height: 10,
-                                        width: 10,
-                                      ),
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  const Flexible(
+                                    child: Text(
+                                        'A green circle means the color and the position is correct.'),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: darkTheme
+                                          ? Colors.white
+                                          : Colors.black,
+                                      borderRadius: BorderRadius.circular(50),
                                     ),
-                                    const SizedBox(
-                                      width: 20,
+                                    child: const SizedBox(
+                                      height: 10,
+                                      width: 10,
                                     ),
-                                    const Text(
-                                        'A green circle means the color and the position is correct.')
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    DecoratedBox(
-                                      decoration: BoxDecoration(
-                                        color: darkTheme
-                                            ? Colors.white
-                                            : Colors.black,
-                                        borderRadius: BorderRadius.circular(50),
-                                      ),
-                                      child: const SizedBox(
-                                        height: 10,
-                                        width: 10,
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      width: 20,
-                                    ),
-                                    const Text(
-                                        'An empty circle means that color is not in the combination at all.')
-                                  ],
-                                )
-                              ],
-                            )
-                          ],
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  const Flexible(
+                                    child: Text(
+                                        'An empty circle means that color is not in the combination at all.'),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
